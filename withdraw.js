571e1432e52b952303a654d7632b2b3ee857441c
BEUGIZER <davidehizuelenekachukwu@gmail.com>
2026-06-29 18:22:13 +0100

    Implement withdraw command for bank transactions

---
+++ withdraw.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require('discord.js');
+const pool = require('../../db/pool');
+const { getAccount } = require('../../db/queries/economy');
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('withdraw')
+    .setDescription('Withdraw coins from your bank')
+    .addIntegerOption((o) => o.setName('amount').setDescription('Amount').setRequired(true).setMinValue(1)),
+  async execute(interaction) {
+    const amount = interaction.options.getInteger('amount');
+    const acc = await getAccount(interaction.guildId, interaction.user.id);
+    if (acc.bank < amount) return interaction.reply({ content: 'Insufficient bank balance.', ephemeral: true });
+    await pool.query('UPDATE economy SET balance = balance + $3, bank = bank - $3 WHERE guild_id=$1 AND user_id=$2', [
+      interaction.guildId,
+      interaction.user.id,
+      amount,
+    ]);
+    await interaction.reply(`🏦 Withdrew **${amount}** coins.`);
+  },
+};
